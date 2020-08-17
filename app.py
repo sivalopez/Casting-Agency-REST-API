c9a1bebfa7dbeeab68929f0ec817efa78c05f06f
Siva Lopez <siva.lopez@gmail.com>
2020-08-17 22:44:36 +1000

    Added a root endpoint for testing.

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from models import setup_db, Movie
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   CORS(app)
+  setup_db(app)
 
   return app
 
-APP = create_app()
+app = create_app()
+
+@app.route('/')
+def test():
+  return "Hello!"
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)

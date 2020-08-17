554ff776ebf2c79dcefda45646e90e78bb3ab6c8
Siva Lopez <siva.lopez@gmail.com>
2020-08-17 22:45:08 +1000

    Connecting to database and created Movie table.

--- models.py
+++ models.py
@@ -1,1 +1,48 @@
+import os
+from sqlalchemy import Column, String, Integer, DateTime, func
+from flask_sqlalchemy import SQLAlchemy
+import json
 
+database_name = "casting_agency"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, unique=True, nullable=False)
+    release_date = Column(DateTime, default=db.func.now())
+
+    def __repr__(self):
+        return format(self)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }

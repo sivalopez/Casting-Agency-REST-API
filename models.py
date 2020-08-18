85f2d60e7fd0d3b974bda0f50aa9005a5851bef1
Siva Lopez <siva.lopez@gmail.com>
2020-08-18 22:28:01 +1000

    Added Movie class and some comments.

--- models.py
+++ models.py
@@ -8,6 +8,10 @@ database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
+'''
+Sets up flask application configuration for SQL Alchemy
+and binds application with SQLAlchemy service.
+'''
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -15,6 +19,9 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+'''
+Movie class for creating and managing movies database table.
+'''
 class Movie(db.Model):
     __tablename__ = 'movies'
 
@@ -45,4 +52,42 @@ class Movie(db.Model):
             'id': self.id,
             'title': self.title,
             'release_date': self.release_date
+        }
+
+'''
+Actor class for creating and managing actors database table.
+'''
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String)
+
+    def __repr__(self):
+        return f'<Actor ID: {self.id}, name: {self.name}, age: {self.age}, gender: {self.gender}>'
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
         }

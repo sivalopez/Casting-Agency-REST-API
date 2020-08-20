010f87c31f486e269ccb032282dcedadbab81fcc
Siva Lopez <siva.lopez@gmail.com>
2020-08-20 22:05:21 +1000

    Implemented GET movies and actors endpoints.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-from models import setup_db, Movie
+from models import setup_db, Movie, Actor
 
 def create_app(test_config=None):
   # create and configure the app
@@ -14,6 +14,46 @@ def create_app(test_config=None):
   return app
 
 app = create_app()
+
+'''
+Handles GET requests for movies.
+This endpoint should return a list of movies.
+'''
+@app.route('/movies', methods=['GET'])
+def getMovies():
+  movies = Movie.query.all()
+
+  # Abort with error when there are no movies returned by the query.
+  if len(movies) == 0:
+    abort(404)
+  
+  # Format all movies.
+  formatted_movies = [movie.format() for movie in movies]
+
+  return jsonify({
+    'success': True,
+    'movies': formatted_movies
+  })
+
+'''
+Handles GET requests for actors.
+This endpoint should return a list of actors.
+'''
+@app.route('/actors', methods=['GET'])
+def getActors():
+  actors = Actor.query.all()
+
+  # Abort with error when there are no actors returned by the query.
+  if len(actors) == 0:
+    abort(404)
+
+  # Format all actors.
+  formatted_actors = [actor.format() for actor in actors]
+
+  return jsonify({
+    'success': True,
+    'actors': formatted_actors
+  })
 
 @app.route('/')
 def test():

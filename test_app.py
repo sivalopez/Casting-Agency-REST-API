7a88686cda4e8fa6771487fbacc25be7d5f86591
Siva Lopez <siva.lopez@gmail.com>
2020-08-27 23:10:33 +1000

    Added some tests for movies endpoints.

--- test_app.py
+++ test_app.py
@@ -1,15 +1,21 @@
-import unittest, sys
+import os, unittest, sys
 import json
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 from src.app import create_app
 from src.models import setup_db, Movie, Actor
 
+# Read the environment variables to get tokens.
+
+CASTING_DIRECTOR_EXPIRED_TOKEN = os.environ.get('CASTING_DIRECTOR_EXPIRED_TOKEN')
+CASTING_DIRECTOR_TOKEN = os.environ.get('CASTING_DIRECTOR_TOKEN')
+EXECUTIVE_PRODUCER_TOKEN = os.environ.get('EXECUTIVE_PRODUCER_TOKEN')
+
 '''
 Test case for Casting Agency.
 '''
 class CastingAgencyTestCase(unittest.TestCase):
-    
     def setUp(self):
         # Initialize the app.
         self.app = create_app()
@@ -29,19 +35,107 @@ class CastingAgencyTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    # def test_post_movies(self):
-    #     newMovie = {'title': 'Whats going on?', 'release_date': '12-06-2021'}
-    #     res = self.client().post('/movies', data=json.dumps(newMovie),
-    #                             headers={'Content-Type': 'application/json'})
-    #     self.assertEqual(res.status_code, 200)
+    def test_post_movies_success(self):
+        newMovie = {'title': 'Sliding Doors7', 'release_date': '12-06-1999'}
+       
+        res = self.client().post('/movies', data=json.dumps(newMovie),
+                                headers={'Content-Type': 'application/json', 'Authorization': 'Bearer ' + str(CASTING_DIRECTOR_TOKEN)})
 
-    def test_get_movies(self):
-        res = self.client().get('/movies')
         json_res = res.json
-        # print('SIVA')
-        print(json_res)
+        print('SL res_json: ' + str(res.json))
 
         self.assertEqual(res.status_code, 200)
+        self.assertTrue(json_res['success'])
+        self.assertIsNotNone(json_res['id'])
+
+    def test_post_movies_error(self):
+        newMovie = {'title': 'Whats going on?', 'release_date': '12-06-2021'}
+        res = self.client().post('/movies', data=json.dumps(newMovie),
+                                headers={'Content-Type': 'application/json'})
+        print('SL test_post_movies_error() res_json: ' + str(res.json))
+        self.assertEqual(res.status_code, 401)
+        self.assertFalse(res.json['success'])
+        self.assertEqual(res.json['error'], 'missing_authorization_header')
+        self.assertEqual(res.json['message'], 'Authorization header is expected.')
+
+    def test_get_movies_success(self):
+        res = self.client().get('/movies')
+        json_res = res.json
+
+        # print('SL test_get_movies_success() res_json: ' + str(json_res))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(json_res['success'])
+        self.assertGreater(len(json_res['movies']), 0)
+    
+    def test_get_movies_error(self):
+        res = self.client().get('/movies/1')
+        json_res = res.json
+
+        self.assertEqual(res.status_code, 200)
+        self.assertFalse(json_res['success'])
+        self.assertEqual(json_res['error'], 405)
+        self.assertEqual(json_res['message'], 'Method Not Allowed')
+
+    def test_edit_movies_success(self):
+        # Add a new movie to edit later.
+        newMovie = {'title': 'Sliding Doors8', 'release_date': '12-06-1999'}
+        post_res = self.client().post('/movies', data=json.dumps(newMovie),
+                        headers={'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + str(CASTING_DIRECTOR_TOKEN)})
+        self.assertTrue(post_res.json['success'])
+        movie_id = post_res.json['id']
+        self.assertIsNotNone(movie_id)
+
+        # Edit the movie
+        res = self.client().patch('/movies/' + str(movie_id), data=json.dumps({'title': 'Sliding Doors Two!'}),
+                        headers={'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + str(CASTING_DIRECTOR_TOKEN)})
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(res.json['success'])
+
+    def test_edit_movies_error(self):
+        # Non existent movie_id
+        res = self.client().patch('/movies/99', data=json.dumps({'title': 'Sliding Doors Three!'}),
+                        headers={'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + str(CASTING_DIRECTOR_TOKEN)})
+
+        self.assertEqual(res.status_code, 200)
+        self.assertFalse(res.json['success'])
+        self.assertEqual(res.json['error'], 404)
+        self.assertEqual(res.json['message'], 'Resource Not Found')
+
+    def test_delete_movies_success(self):
+        # Add a new movie to edit later.
+        newMovie = {'title': 'Sliding Doors2', 'release_date': '12-06-1999'}
+        post_res = self.client().post('/movies', data=json.dumps(newMovie),
+                        headers={'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + str(EXECUTIVE_PRODUCER_TOKEN)})
+
+
+        self.assertTrue(post_res.json['success'])
+        movie_id = post_res.json['id']
+        self.assertIsNotNone(movie_id)
+
+        res = self.client().delete('/movies/' + str(movie_id),
+                        headers={'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + str(EXECUTIVE_PRODUCER_TOKEN)})
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(res.json['success'])
+        self.assertIsNotNone(res.json['id'])
+
+    def test_delete_movies_error(self):
+        # Delete a non-existent movie_id
+        res = self.client().delete('/movies/99',
+                        headers={'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + str(EXECUTIVE_PRODUCER_TOKEN)})
+
+        self.assertEqual(res.status_code, 200)
+        self.assertFalse(res.json['success'])
+        self.assertEqual(res.json['error'], 404)
+        self.assertEqual(res.json['message'], 'Resource Not Found')
 
 '''
 Provides command line interface to the test script.

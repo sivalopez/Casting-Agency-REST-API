073f5cc96c8b9b1f037a48154eac2b5de91142a3
Siva Lopez <siva.lopez@gmail.com>
2020-08-30 15:02:06 +1000

    Changed DATABASE_NAME to DATABASE_PATH.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_NAME=casting_agency
+export DATABASE_URL=postgres://localhost:5432/casting_agency
 export AUTH0_DOMAIN='fsnd-capstone-silo.au.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='casting_agency_api'
